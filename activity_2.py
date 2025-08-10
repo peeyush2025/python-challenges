91b0c6686525a81dd783180cc398d1b809cc26df
peeyush2025 <sudhakarln82@gmail.com>
2025-08-10 19:35:18 +0530

    Add files via upload

---
+++ activity_2.py
@@ -0,0 +1,9 @@
+# divisible number
+print("enter a number(Numerator)")
+num_n=int(input())
+print("enter a number(Denominator)")
+num_d=int(input())
+if num_n% num_d==0:
+    print("num is divisible")
+else:
+    print("num is not divisble")
